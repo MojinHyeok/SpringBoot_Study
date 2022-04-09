348a15d49a99d0f04dccd41ad5df863d39bb3b20
모진혁 <gkgk246@naver.com>
2022-04-09 21:10:14 +0900

    spring_batch 메타테이블 엿보기

--- spring_batch/src/main/java/com/backend/batch/job/SimpleJobConfiguration.java
+++ spring_batch/src/main/java/com/backend/batch/job/SimpleJobConfiguration.java
@@ -3,8 +3,11 @@ package com.backend.batch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,15 +25,30 @@ public class SimpleJobConfiguration {
 	@Bean
 	public Job simpleJob() {
 		return jobBuilderFactory.get("simpleJob")//simpleJob이라는 이름으로 Batch job생성
-				.start(simpleStep1())
+				.start(simpleStep1(null))
+				.next(simpleStep2(null))
 				.build();
 	}
 
 	@Bean
-	public Step simpleStep1() {
-		return stepBuilderFacory.get("simpleStep1")//simpleStep1이라는 이름으로 Batch Step을 생성합니다.
+	@JobScope
+	public Step simpleStep1(@Value("#{jobParameters[requestDate]}")String requestDate) {
+		return stepBuilderFacory.get("simpleStep1")
+				.tasklet((contribution, chunkContext)->{
+					log.info("===== This is Step2");
+					log.info(">>>>> requestDate ={}",requestDate);
+					return RepeatStatus.FINISHED;
+				})
+				.build();
+	}
+
+	@Bean
+	@JobScope
+	public Step simpleStep2(@Value("#{jobParameters[requestDate]}")String requestDate) {
+		return stepBuilderFacory.get("simpleStep2")//simpleStep2이라는 이름으로 Batch Step을 생성합니다.
 				.tasklet((contribution,chunkContext) -> {
-					log.info("===== This is Step1");
+					log.info("===== This is Step2");
+					log.info(">>>>> requestDate ={}",requestDate);
 					return RepeatStatus.FINISHED;
 				}).build();
 	}
