ae413fdc08b2e1171b48fa3e92c821313da3ba57
모진혁 <gkgk246@naver.com>
2023-08-19 21:45:25 +0900

    feat : 서블릿 컨텍스트 띄우기

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,12 +2,19 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+	public static void main(String[] args){
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//        ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+		//톰캣 외의 다른 서블릿 컨테이너를 받을 수 있도록 일관된 방식으로 지원하기 위해 추상화를 진행한 상태입니다.
+		WebServer webServer = serverFactory.getWebServer();
+		//톰캣 서블릿컨테이너 실행
+		webServer.start();
 	}
 
 }
