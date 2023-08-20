ef5773a0bd0f7807369e49157dbee06e0789413e
모진혁 <gkgk246@naver.com>
2023-08-20 12:02:31 +0900

    feat : 프론트컨트롤러구현

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -25,17 +29,27 @@ public class HellobootApplication {
 //			public void onStartup(ServletContext servletContext) throws ServletException {
 //			}
 //		});
-		//ServeletContextInitializer는 @FunctionalInterface 임으로 람다식으로 대체가 가능
+		//ServeletContextInitializer는 @FunctionalInterface 임으로 람다식으로 대체가 가능합니다.
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			// 서블릿을 ServletContext에 추가합니다. "hello"는 서블릿의 이름입니다.
+			HelloController helloController = new HelloController();
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//웹 요청에 해당하는 3가지 상태코드, 헤더, 바디
-					resp.setStatus(200);
-					resp.setHeader("Content-Type","text/plain");
-					resp.getWriter().println("Hello Servlet");
+					//여기에서 인증과 보안 다국어 공통기능을 처리한다고 가정.
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+						String ret = helloController.hello(name);
+						// 웹 요청에 대한 응답을 처리하는 메소드입니다.
+						//웹 요청에 해당하는 3가지 상태코드, 헤더, 바디
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);//String보다는 Spring에서 제공하는 단어사전과 같은 기능을 통해 수정.
+						resp.getWriter().println(ret);
+					}else{
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");// "/hello" 경로로 들어온 요청에 이 서블릿을 매핑합니다.
+			}).addMapping("/*");
 		});
 		//톰캣 서블릿컨테이너 실행
 		webServer.start();
